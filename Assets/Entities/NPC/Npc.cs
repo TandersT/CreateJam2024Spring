c97e9d7f499b69ac2989081926d7c40eed37119c
Acroh <fmalle20@student.aau.dk>
2024-03-30 07:19:30 +0100

    Dialog and UI partially implemented
    Dialog has logic but no functionality. Will display on the screen correctly. UI doesn't disappear after appearing. Peasants does not have dialog. General dialog, greetings and role dialog not implemented yet

--- Assets/Entities/NPC/Npc.cs
+++ Assets/Entities/NPC/Npc.cs
@@ -32,6 +32,8 @@ public partial class Npc : CharacterBody2D
     Texture2D[] NpcIcons;
     RoleEnum Role;
     ProficiencyEnum Proficiency;
+    string NPCName;
+    bool FirstInteract;
 
 
     Label RoleLabel => GetNode<Label>("%RoleLabel");
@@ -73,6 +75,113 @@ public partial class Npc : CharacterBody2D
     public void OnInteractedWith()
     {
         GD.Print(Position.DistanceSquaredTo(Global.Player.Position));
+        var dialogUI = GetNode<DialogUI>("/root/Main/UI/DialogUI");
+        dialogUI.Visible = true;
+        var dialogText = dialogUI.GetNode<Label>("%DialogLabel");
+
+        if (Role == RoleEnum.Programmer)
+        {
+            var programmerLists = GetNode<ProgrammerRole>("/root/Main/Roles/Programmer");
+            int randomIndex;
+            switch (Proficiency)
+            {
+                case ProficiencyEnum.Novice:
+                    randomIndex = GD.RandRange(0, programmerLists.Novice.Count-1);
+                    dialogText.Text = programmerLists.Novice[randomIndex];
+                    break;
+                case ProficiencyEnum.Intermediate:
+                    randomIndex = GD.RandRange(0, programmerLists.Intermediate.Count-1);
+                    dialogText.Text = programmerLists.Intermediate[randomIndex];
+                    break;
+                case ProficiencyEnum.Expert:
+                    randomIndex = GD.RandRange(0, programmerLists.Expert.Count-1);
+                    dialogText.Text = programmerLists.Expert[randomIndex];
+                    break;
+            }
+        }
+
+        if (Role == RoleEnum.GraphicsArtist)
+        {
+            var ArtistLists = GetNode<GraphicsArtist>("/root/Main/Roles/GraphicsArtist");
+            int randomIndex;
+            switch (Proficiency)
+            {
+                case ProficiencyEnum.Novice:
+                    randomIndex = GD.RandRange(0, ArtistLists.Novice.Count-1);
+                    dialogText.Text = ArtistLists.Novice[randomIndex];
+                    break;
+                case ProficiencyEnum.Intermediate:
+                    randomIndex = GD.RandRange(0, ArtistLists.Intermediate.Count-1);
+                    dialogText.Text = ArtistLists.Intermediate[randomIndex];
+                    break;
+                case ProficiencyEnum.Expert:
+                    randomIndex = GD.RandRange(0, ArtistLists.Expert.Count-1);
+                    dialogText.Text = ArtistLists.Expert[randomIndex];
+                    break;
+            }
+        }
+
+        if (Role == RoleEnum.SoundDesigner)
+        {
+            var SoundLists = GetNode<SoundDesignerRole>("/root/Main/Roles/SoundDesigner");
+            int randomIndex;
+            switch (Proficiency)
+            {
+                case ProficiencyEnum.Novice:
+                    randomIndex = GD.RandRange(0, SoundLists.Novice.Count-1);
+                    dialogText.Text = SoundLists.Novice[randomIndex];
+                    break;
+                case ProficiencyEnum.Intermediate:
+                    randomIndex = GD.RandRange(0, SoundLists.Intermediate.Count-1);
+                    dialogText.Text = SoundLists.Intermediate[randomIndex];
+                    break;
+                case ProficiencyEnum.Expert:
+                    randomIndex = GD.RandRange(0, SoundLists.Expert.Count-1);
+                    dialogText.Text = SoundLists.Expert[randomIndex];
+                    break;
+            }
+        }
+
+        if (Role == RoleEnum.Peasent)
+        {
+            //Change the things to general stuff.
+            var SoundLists = GetNode<SoundDesignerRole>("/root/Main/Roles/SoundDesigner");
+            int randomIndex;
+            randomIndex = GD.RandRange(0, SoundLists.Novice.Count-1);
+            dialogText.Text = SoundLists.Novice[randomIndex];
+        }
+    }
+
+    public void DialogDecider()
+    {
+        //Decides what type of info is given
+        int randomNumber = GD.RandRange(0, 100);
+        if (FirstInteract == true)
+        {
+            if (randomNumber < 70)
+            {
+                //Normal Greeting
+            }
+        }
+        else
+        {
+            if (randomNumber < 25)
+            {
+                //Give general info
+            }
+            else if (randomNumber >= 25 && randomNumber < 50)
+            {
+                //Give role Info
+            }
+            else if (randomNumber >= 50 && randomNumber < 75)
+            {
+                //Give proficiency Info
+            }
+            else
+            {
+                //Give Info on project
+            }
+        }
     }
 
     public void RangeStatusUpdated()
