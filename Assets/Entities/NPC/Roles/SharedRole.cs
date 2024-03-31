e3aa93f32eef5fec7f65c3233b0e73e7f5f1c650
MephistoPan <mephistopan@gmail.com>
2024-03-31 03:16:31 +0200

    Updated the SharedRole dialog texts

--- Assets/Entities/NPC/Roles/SharedRole.cs
+++ Assets/Entities/NPC/Roles/SharedRole.cs
@@ -6,17 +6,29 @@ public abstract class SharedRole : IProficienct
 	public List<string> General { get; set; } = new List<string>
 {
 	"I’ve been wondering a lot lately: How do bats piss upside down?",
-	"A friend of mine who’s a priest told me he wasn’t coming because he felt evil forces emanating from this place. You wouldn’t know what he’s talking about, would you?",
+	"A friend of mine told me he was feeling evil forces emanating from this place. Would you happen to know what he’s talking about, perchance?",
 	"Why is the weather cloudy all the time?",
-	"Why are you talking to me?",
+	"Are you talking to me?",
 	"Making games is so easy that I can do it with just one hand. And it’s not because I’m missing an arm.",
 	"You look like a very competitive person.",
-	"My programmer cousin got hired for developing a security system for the local bank. Pretty cool, huh?",
 	"I heard there’s free snacks here.",
 	"Wake up! Just kidding… Or am I?",
 	"I should’ve slept more…",
 	"Can you feel it? A sudden urge for ice cream. No? Is it just me?",
-	"I wonder what it is like working in a submarine…"
+	"I wonder what it is like working and living in a submarine for a few weeks straight…",
+	"I need more beer…",
+	"I’ve become lactose intolerant ever since my father left to get some milk.",
+	"Looks like someone’s selling hotdogs outside.",
+	"Do you smell that? A faint smell of ashes… Something evil’s afoot! I’m sure of it!",
+	"I heard there’s a circus getting set up nearby. Do you like clowns?",
+	"Imma steal your steel while you stand still! I dare you to say that 5 times in a row and FAST.",
+	"Did you know that african buffaloes make decisions by voting? The more you know…",
+	"Have you ever thought about what the world would look like if all conflicts were solved via dance contests? We should let disputes be settled on the dance floor!",
+	"I’m running out of ideas. Do you have any to share?",
+	"Could you remind me how long we have until the jam is over?",
+	"WHERE’S THE CREATE JAM JAM??!",
+	"I’m telling you that pyramids were definitely built by aliens."
+
 };
 	public List<string> Greetings { get; set; } = new List<string>
 {
@@ -46,7 +58,7 @@ public abstract class SharedRole : IProficienct
 	{
 		"Frick off",
 		"u are dumb",
-		"no talk more",
+		"no talk no more",
 		"get reckt",
 		"u dumb",
 	};
